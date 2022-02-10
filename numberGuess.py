261e20b5bab1d4138bec5b2390c74cc36bf62a4c
prateek <prateeksinha2404@gmail.com>
2022-02-11 00:09:19 +0530

    project 2

---
+++ numberGuess.py
@@ -0,0 +1,21 @@
+import random
+
+
+def numberGuess(number):
+
+    guess = 0
+    input1 = None
+    while(input1 != number):
+        input1 = int(input("Enter your guess : \n"))
+        guess += 1
+        if input1 == number:
+            print(f"Bravo!!! You guessed the right num in {guess} guesses!")
+        elif input1 > number:
+            print("Oops! Wrong Choice. Enter a smaller number..")
+        else:
+            print("Oops! Wrong Choice. Enter a larger number..")
+
+
+number = random.randint(1, 100)
+print(number)
+numberGuess(number)
